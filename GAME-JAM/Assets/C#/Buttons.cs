4df83d70b602984e00b9ccdbc3d34b55f76d4c6a
Zelerv0 <lkoshak.06@gmail.com>
2025-09-09 23:12:55 +0200

    Buttons and pause menu

---
+++ GAME-JAM/Assets/C#/Buttons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void OnPress()
+    {
+        SceneManager.LoadScene("Labyrinth_Testing");
+    }
+    public void ContinueButton()
+    {
+        this.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
