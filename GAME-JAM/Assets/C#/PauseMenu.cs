7cfb3d358a917e7a00ebd3624428d6e021e948ba
Zelerv0 <lkoshak.06@gmail.com>
2025-09-12 01:18:32 +0200

    Art and fix UI

--- GAME-JAM/Assets/C#/PauseMenu.cs
+++ GAME-JAM/Assets/C#/PauseMenu.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject deathScreen;
+
+    public static PauseMenu thisPauseMenu;
+
+    private void Start()
+    {
+        thisPauseMenu = this;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -12,5 +21,10 @@ public class PauseMenu : MonoBehaviour
             pauseMenu.SetActive(true);
             Time.timeScale = 0f;
         }
+    }
+    public void DeathScreen()
+    {
+        deathScreen.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
